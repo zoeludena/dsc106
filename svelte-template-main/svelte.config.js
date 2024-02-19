77815fb9a14e69810d424ed10eb5fef3df2bb0c1
Zoe Ludena <zludena@ucsd.edu>
2024-02-19 09:07:09 -0800

    Attempting to fix github page

--- svelte-template-main/svelte.config.js
+++ svelte-template-main/svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : '/dsc106-Project3'//process.env.BASE_PATH
 		}
 	}
 };
